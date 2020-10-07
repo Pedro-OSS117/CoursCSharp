78ee731e5b1927b9d7ba7c37e0527a338423a628
Pedro-OSS117 <pierreproudhon@gmail.com>
2020-10-07 15:37:37 +0200

    Ajout cours variables

--- LesVariables/LesTypes/Program.cs
+++ LesVariables/LesTypes/Program.cs
@@ -46,10 +46,68 @@ namespace LesTypes
             int valueToByte = 355;
             byte byteValue = (byte)valueToByte;
             Console.WriteLine("valueToByte : " + valueToByte + " byteValue : " + byteValue);
-
             
             int valueChar = 'v';
             Console.WriteLine("valueChar : " + valueChar);
+
+            // Les operateurs numeriques : +, -, /, *, %
+            int testOperateur1Int = 10; 
+            int testOperateur2Int = 15; 
+            float testOperateur2Float = 15.6f; 
+            double testOperateur2Double = 15.6;
+
+            int resultOperation = testOperateur1Int + testOperateur2Int;  
+            resultOperation = testOperateur1Int + (int)testOperateur2Float+ (int)testOperateur2Float;  
+            Console.WriteLine("resultOperation : " + resultOperation);
+            resultOperation = (int)(testOperateur1Int + testOperateur2Float+ testOperateur2Float);  
+            Console.WriteLine("resultOperation : " + resultOperation);
+
+
+            int resulatOperationDiv = testOperateur1Int / testOperateur2Int;
+            Console.WriteLine("resulatOperationDiv : " + resulatOperationDiv);
+
+            resulatOperationDiv = (int)((testOperateur2Float + testOperateur1Int) / testOperateur2Int);
+
+            // Exemple de decoupage d'instruction => les 5 lignes en dessous representent
+            // l'instruction au dessus mais découpée.
+            float tmp = testOperateur2Float;
+            tmp = tmp + testOperateur1Int;
+            tmp = tmp / testOperateur2Int;
+            int tmpInt = (int)tmp;
+            resulatOperationDiv = tmpInt;
+            
+            tmp = testOperateur2Float;
+            tmp += testOperateur1Int;
+            tmp /= testOperateur2Int;
+            tmpInt = (int)tmp;
+            resulatOperationDiv = tmpInt;
+
+            Console.WriteLine("resulatOperationDiv : " + resulatOperationDiv);
+
+            resulatOperationDiv = (int)testOperateur2Float / testOperateur1Int;
+            Console.WriteLine("resulatOperationDiv : " + resulatOperationDiv);
+            resulatOperationDiv = (int)(testOperateur2Float / testOperateur1Int);
+            Console.WriteLine("resulatOperationDiv : " + resulatOperationDiv);
+
+            float resulatOperationDivFloat = testOperateur1Int / testOperateur2Int;
+            Console.WriteLine("resulatOperationDivFloat : " + resulatOperationDivFloat);
+
+            int time = 450;
+            int minutes = time / 60;
+            //int secondes = time - minutes * 60;
+            int secondes = time % 60;
+            Console.WriteLine("time : " + minutes + " : " + secondes);
+
+            string line = "Je declare et initialise ma string comme ça !";
+            string line2 = "J'ai concatené cette partie là.";
+
+            // Concatenation
+            // \n dans le contenu dans la string permet de faire un retour à la ligne 
+            Console.WriteLine(line + line2);
+            line += line2;
+            Console.WriteLine(line + line2);
+            line = line2;
+            Console.WriteLine(line + line2);
         }
     }
 }
