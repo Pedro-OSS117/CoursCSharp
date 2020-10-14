6fc10aa65a7465b59bfab1de08040273a007ee7b
Pedro-OSS117 <pierreproudhon@gmail.com>
2020-10-14 15:50:24 +0200

    Update Exo Instructions Conditionnelles

---
+++ LesInstructionsConditionnelles/ExoCours/SeasonalTest/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace SeasonalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string monthAsked = Console.ReadLine();
+            string savedMonthAsked = monthAsked;
+
+            monthAsked = monthAsked.ToLower();
+            monthAsked = monthAsked.Replace('é', 'e');
+            monthAsked = monthAsked.Replace('û', 'u');
+            
+            Console.WriteLine("Avant modifs : " + savedMonthAsked + " Après modifs : " + monthAsked);            
+
+            if(monthAsked == "janvier" || monthAsked == "fevrier" || monthAsked == "decembre")
+            {
+                Console.WriteLine("Le mois " + savedMonthAsked + " est en Hiver!");
+            }
+            else if(monthAsked == "mars" || monthAsked == "avril" || monthAsked == "mai")
+            {
+                Console.WriteLine("Le mois " + savedMonthAsked + " est en Printemps!");
+            }
+            else if(monthAsked == "juin" || monthAsked == "juillet" || monthAsked == "aout")
+            {
+                Console.WriteLine("Le mois " + savedMonthAsked + " est en Ete!");
+            }
+            else if(monthAsked == "septembre" || monthAsked == "octobre" || monthAsked == "novembre")
+            {
+                Console.WriteLine("Le mois " + savedMonthAsked + " est en Automne!");
+            }
+            else
+            {
+                Console.WriteLine("Le mois " + savedMonthAsked + " n'est pas un mois sans deconner!");
+            }
+
+            string season = "";
+
+            switch(monthAsked)
+            {
+                case "janvier":
+                case "fevrier":
+                case "decembre":
+                    season = "Hiver";
+                    break;
+                case "mars":
+                case "avril":
+                case "mai":
+                    season = "Printemps";
+                    break;
+                case "juin":
+                case "juillet":
+                case "aout":
+                    season = "Ete";
+                    break;
+                case "septembre":
+                case "octobre":
+                case "novembre":
+                    season = "Automne";
+                    break;
+                default:
+                    Console.WriteLine("Le mois " + savedMonthAsked + " n'est pas un mois sans deconner!");
+                    break;
+                    
+            }
+            
+            //if(!string.IsNullOrEmpty(season))
+            if(season != "")
+            {
+                Console.WriteLine("Le mois " + savedMonthAsked + " est en " + season + "!");
+            }
+        }
+    }
+}
---
+++ LesInstructionsConditionnelles/ExoCours/StateLifeEntity/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StateLifeEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte life = 100;
+
+            bool isInvincible = false;
+            bool isHurtable = true;
+
+            byte damages = 50;
+
+            Console.WriteLine("Life Bob is : " + life + " and his state invicible is : " + isInvincible);
+            if( life > 0 && isHurtable)
+            {
+                life -= damages;
+            }
+            Console.WriteLine("Life Bob is : " + life + " and his state invicible is : " + isInvincible);
+
+            isInvincible = true;            
+            if( life > 0 && !isInvincible)
+            {
+                life -= damages;
+            }
+            Console.WriteLine("Life Bob is : " + life + " and his state invicible is : " + isInvincible);
+        }
+    }
+}
